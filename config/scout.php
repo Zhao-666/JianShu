0fb6a1929c8500ec183e7b7ae48ff0ed725fd972
Zhao-666 <2268699370@qq.com>
2018-04-03 23:30:31 +0800

    9-8 创建post索引，导入post数据

--- config/scout.php
+++ config/scout.php
@@ -90,7 +90,9 @@ return [
 
     'elasticsearch' => [
         'index' => env('ELASTICSEARCH_INDEX', 'laravel54'),
-        'hosts' => env('ELASTICSEARCH_HOST', 'http://127.0.0.1:9200'),
+        'hosts' => [
+            env('ELASTICSEARCH_HOST', 'http://127.0.0.1:9200'),
+        ],
     ],
 
 ];
