e897a9034832822fd65b033d3c8d0d116ac23899
Zhao-666 <2268699370@qq.com>
2018-04-08 23:30:49 +0800

    10-3 使用ORM定义Fans关系表，提供查询粉丝和关注的查询接口

--- app/User.php
+++ app/User.php
@@ -9,4 +9,43 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'password', 'email'
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class, 'user_id', 'id');
+    }
+
+    public function fans()
+    {
+        return $this->hasMany(Fan::class, 'star_id', 'id');
+    }
+
+    public function stars()
+    {
+        return $this->hasMany(Fan::class, 'fan_id', 'id');
+    }
+
+    public function doFan($uid)
+    {
+        $fan = new Fan();
+        $fan->star_id = $uid;
+        return $this->stars()->save($fan);
+    }
+
+    public function doUnFan($uid)
+    {
+        $fan = new Fan();
+        $fan->star_id = $uid;
+        return $this->stars()->delete($fan);
+    }
+
+    public function hasFan($uid)
+    {
+        return $this->fans()->where('fan_id', $uid)->count();
+    }
+
+    public function hasStar($uid)
+    {
+        return $this->fans()->where('star_id', $uid)->count();
+    }
 }
