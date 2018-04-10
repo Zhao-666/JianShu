caae3f2e6240525fc416bb5783b436f38517532b
Zhao-666 <2268699370@qq.com>
2018-04-11 00:11:07 +0800

    10-5 使用多对多模型实现个人中心逻辑

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 
+use App\User;
+
 class UserController extends Controller
 {
     public function setting()
@@ -15,9 +17,19 @@ class UserController extends Controller
 
     }
 
-    public function show()
+    public function show(User $user)
     {
-        return view('user.show');
+        $user = User::withCount(['stars', 'fans', 'posts'])->find($user->id);
+
+        $posts = $user->posts()->orderBy('created_at', 'desc')->take(10)->get();
+
+        $stars = $user->stars;
+        $suser = User::whereIn('id', $stars->pluck('star_id'))->withCount(['stars', 'fans', 'posts'])->get();
+
+        $fans = $user->fans;
+        $fuser = User::whereIn('id', $fans->pluck('fan_id'))->withCount(['stars', 'fans', 'posts'])->get();
+
+        return view('user.show', compact('user', 'posts', 'suser', 'fuser'));
     }
 
     public function fan()
