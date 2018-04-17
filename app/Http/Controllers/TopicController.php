463e02aa28f45a2ad8ba077e45ebcee44a50731f
Zhao-666 <2268699370@qq.com>
2018-04-17 22:15:16 +0800

    FIX BUG

---
+++ app/Http/Controllers/TopicController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Topic;
+
+class TopicController extends Controller
+{
+    public function show(Topic $topic)
+    {
+        return view('topic/show');
+    }
+
+    public function submit(Topic $topic)
+    {
+        return;
+    }
+}
